35eedc1942081d45a4d9b479b48ad95ed70bb8e6
Jesse Nickles <jesse@littlebizzy.com>
2017-10-22 19:26:33 -0700

    Add files via upload

---
+++ force-https.php
@@ -0,0 +1,28 @@
+<?php
+/*
+Plugin Name: Force HTTPS
+Plugin URI: https://www.littlebizzy.com/plugins/force-https
+Description: Redirects all HTTP requests to the HTTPS version and fixes all insecure static resources by implementing relative URLs without altering the database.
+Version: 1.0.0
+Author: LittleBizzy
+Author URI: https://www.littlebizzy.com
+License: GPL3
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+*/
+
+// Avoid script calls via plugin URL
+if (!function_exists('add_action'))
+	die;
+
+// This plugin constants
+define('FHTTPS_FILE', __FILE__);
+define('FHTTPS_PATH', dirname(FHTTPS_FILE));
+define('FHTTPS_VERSION', '1.0.0');
+
+// Early check
+if (defined('FORCE_SSL') && !FORCE_SSL)
+	return;
+
+// Load main class
+require_once(FHTTPS_PATH.'/core/core.php');
+FHTTPS_Core::instance();
