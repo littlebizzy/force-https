f2b7cb473c826fb05cb803411c2a22b9285dfe99
Jesse Nickles <jesse@littlebizzy.com>
2017-10-24 11:24:37 -0700

    Update force-https.php

--- force-https.php
+++ force-https.php
@@ -2,12 +2,13 @@
 /*
 Plugin Name: Force HTTPS
 Plugin URI: https://www.littlebizzy.com/plugins/force-https
-Description: Redirects all HTTP requests to the HTTPS version and fixes all insecure static resources by implementing relative URLs without altering the database.
+Description: Redirects all HTTP requests to the HTTPS version and fixes all insecure static resources by implementing secure URLs without altering the database.
 Version: 1.0.2
 Author: LittleBizzy
 Author URI: https://www.littlebizzy.com
-License: GPL3
+License: GPLv3
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
+Prefix: FHTTPS
 */
 
 // Admin Notices module
@@ -18,11 +19,11 @@ FHTTPS_Admin_Notices::instance(__FILE__);
  * Plugin code
  */
 
-// Avoid script calls via plugin URL
+// Block direct calls
 if (!function_exists('add_action'))
 	die;
 
-// This plugin constants
+// Plugin constants
 define('FHTTPS_FILE', __FILE__);
 define('FHTTPS_PATH', dirname(FHTTPS_FILE));
 define('FHTTPS_VERSION', '1.0.2');
