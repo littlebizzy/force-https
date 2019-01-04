7d4688d35eee9113761211a62b85cab1426bf5d1
Pau <pauiglesias@gmail.com>
2019-01-04 09:32:43 +0100

    Updated main plugin file.

--- force-https.php
+++ force-https.php
@@ -3,40 +3,25 @@
 Plugin Name: Force HTTPS
 Plugin URI: https://www.littlebizzy.com/plugins/force-https
 Description: Redirects all HTTP requests to the HTTPS version and fixes insecure links and resources without altering the database (also works with CloudFlare).
-Version: 1.2.0
+Version: 1.3.0
 Author: LittleBizzy
 Author URI: https://www.littlebizzy.com
 License: GPLv3
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
+PBP Version: 1.1.0
+WC requires at least: 3.3
+WC tested up to: 3.5
 Prefix: FHTTPS
 */
 
-// Admin Notices module
-require_once dirname(__FILE__).'/admin-notices.php';
-FHTTPS_Admin_Notices::instance(__FILE__);
-
-/**
- * Admin Notices Multisite check
- * Uncomment //return to disable this plugin on Multisite installs
- */
-require_once dirname(__FILE__).'/admin-notices-ms.php';
-if (false !== \LittleBizzy\ForceHTTPS\Admin_Notices_MS::instance(__FILE__)) {
-	//return;
-}
-
-// Block direct calls
-if (!function_exists('add_action'))
-	die;
+// Plugin namespace
+namespace LittleBizzy\ForceHTTPS;
 
 // Plugin constants
-define('FHTTPS_FILE', __FILE__);
-define('FHTTPS_PATH', dirname(FHTTPS_FILE));
-define('FHTTPS_VERSION', '1.2.0');
+const FILE = __FILE__;
+const PREFIX = 'fhttps';
+const VERSION = '1.3.0';
 
-// Early check
-if (defined('FORCE_SSL') && !FORCE_SSL)
-	return;
-
-// Load main class
-require_once(FHTTPS_PATH.'/core/core.php');
-FHTTPS_Core::instance();
+// Boot
+require_once dirname(FILE).'/helpers/boot.php';
+Helpers\Boot::instance(FILE);
