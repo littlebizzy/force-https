d18320f4e71ea2bc214ac26c0319ab28900748b4
Pau <pauiglesias@gmail.com>
2019-02-21 18:32:12 +0100

    Updated main plugin file.

--- force-https.php
+++ force-https.php
@@ -3,12 +3,12 @@
 Plugin Name: Force HTTPS
 Plugin URI: https://www.littlebizzy.com/plugins/force-https
 Description: Redirects all HTTP requests to the HTTPS version and fixes insecure links and resources without altering the database (also works with CloudFlare).
-Version: 1.3.0
+Version: 1.4.0
 Author: LittleBizzy
 Author URI: https://www.littlebizzy.com
 License: GPLv3
-License URI: http://www.gnu.org/licenses/gpl-3.0.html
-PBP Version: 1.1.0
+License URI: https://www.gnu.org/licenses/gpl-3.0.html
+PBP Version: 1.2.0
 WC requires at least: 3.3
 WC tested up to: 3.5
 Prefix: FHTTPS
@@ -20,7 +20,8 @@ namespace LittleBizzy\ForceHTTPS;
 // Plugin constants
 const FILE = __FILE__;
 const PREFIX = 'fhttps';
-const VERSION = '1.3.0';
+const VERSION = '1.4.0';
+const REPO = 'littlebizzy/force-https';
 
 // Boot
 require_once dirname(FILE).'/helpers/boot.php';
