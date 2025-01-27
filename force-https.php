bf859f6694b679fc14b300600af64226c8185408
Jesse Nickles <jesse@littlebizzy.com>
2025-01-27 17:50:58 +0700

    Update force-https.php

--- force-https.php
+++ force-https.php
@@ -3,7 +3,7 @@
 Plugin Name: Force HTTPS
 Plugin URI: https://www.littlebizzy.com/plugins/force-https
 Description: HTTPS enforcement for WordPress
-Version: 2.0.4
+Version: 2.1.0
 Requires PHP: 7.0
 Author: LittleBizzy
 Author URI: https://www.littlebizzy.com
@@ -25,7 +25,8 @@ add_filter( 'gu_override_dot_org', function( $overrides ) {
 }, 999 );
 
 // force https redirection for all non-https requests
-add_action( 'init', function() {
+add_action( 'init', 'force_https_redirect_non_https', 10 );
+function force_https_redirect_non_https() {
     if ( ! is_ssl() && ! is_admin() && PHP_SAPI !== 'cli' ) {
         if ( ! headers_sent() ) {
             $redirect_url = home_url( add_query_arg( null, null ) );
@@ -33,10 +34,10 @@ add_action( 'init', function() {
             exit;
         }
     }
-}, 10 );
+}
 
 // force https on all urls by replacing http:// with https://
-function fhttps_securize_url( $url ) {
+function force_https_securize_url( $url ) {
     if ( strpos( $url, 'http://' ) === 0 ) {
         return set_url_scheme( $url, 'https' );
     }
@@ -44,38 +45,40 @@ function fhttps_securize_url( $url ) {
 }
 
 // apply https to all relevant wordpress filters
-add_filter( 'script_loader_src', 'fhttps_securize_url', 20 );
-add_filter( 'style_loader_src', 'fhttps_securize_url', 20 );
-add_filter( 'wp_get_attachment_url', 'fhttps_securize_url', 20 );
-add_filter( 'the_permalink', 'fhttps_securize_url', 20 );
-add_filter( 'post_link', 'fhttps_securize_url', 20 );
-add_filter( 'page_link', 'fhttps_securize_url', 20 );
-add_filter( 'term_link', 'fhttps_securize_url', 20 );
-add_filter( 'home_url', 'fhttps_securize_url', 20 );
-add_filter( 'site_url', 'fhttps_securize_url', 20 );
-add_filter( 'network_site_url', 'fhttps_securize_url', 20 );
-add_filter( 'network_home_url', 'fhttps_securize_url', 20 );
-add_filter( 'template_directory_uri', 'fhttps_securize_url', 20 );
-add_filter( 'stylesheet_directory_uri', 'fhttps_securize_url', 20 );
-add_filter( 'get_avatar_url', 'fhttps_securize_url', 20 );
-add_filter( 'rest_url', 'fhttps_securize_url', 20 );
+add_filter( 'script_loader_src', 'force_https_securize_url', 20 );
+add_filter( 'style_loader_src', 'force_https_securize_url', 20 );
+add_filter( 'wp_get_attachment_url', 'force_https_securize_url', 20 );
+add_filter( 'the_permalink', 'force_https_securize_url', 20 );
+add_filter( 'post_link', 'force_https_securize_url', 20 );
+add_filter( 'page_link', 'force_https_securize_url', 20 );
+add_filter( 'term_link', 'force_https_securize_url', 20 );
+add_filter( 'home_url', 'force_https_securize_url', 20 );
+add_filter( 'site_url', 'force_https_securize_url', 20 );
+add_filter( 'network_site_url', 'force_https_securize_url', 20 );
+add_filter( 'network_home_url', 'force_https_securize_url', 20 );
+add_filter( 'template_directory_uri', 'force_https_securize_url', 20 );
+add_filter( 'stylesheet_directory_uri', 'force_https_securize_url', 20 );
+add_filter( 'get_avatar_url', 'force_https_securize_url', 20 );
+add_filter( 'rest_url', 'force_https_securize_url', 20 );
 
 // ensure all urls in the upload directory use https
-add_filter( 'upload_dir', function( $uploads ) {
+add_filter( 'upload_dir', 'force_https_fix_upload_dir', 20 );
+function force_https_fix_upload_dir( $uploads ) {
     $uploads['url'] = set_url_scheme( $uploads['url'], 'https' );
     $uploads['baseurl'] = set_url_scheme( $uploads['baseurl'], 'https' );
     return $uploads;
-} );
+}
 
 // apply https to all elements and attributes that can contain urls
-add_filter( 'the_content', function( $content ) {
-    $content = fhttps_convert_urls_to_https( $content );
-    $content = fhttps_convert_inline_styles_to_https( $content );
+add_filter( 'the_content', 'force_https_process_content', 20 );
+function force_https_process_content( $content ) {
+    $content = force_https_convert_urls_to_https( $content );
+    $content = force_https_convert_inline_styles_to_https( $content );
     return $content;
-}, 20 );
+}
 
 // helper function: convert all resource and hyperlink urls to https
-function fhttps_convert_urls_to_https( $content ) {
+function force_https_convert_urls_to_https( $content ) {
     return preg_replace_callback(
         '#(<(?:img|iframe|embed|source|script|link|meta|video|audio|track|object|form|area|input|button|a)[^>]+(?:src|srcset|data-src|data-href|action|poster|content|style|href|manifest)=["\'])(http://)([^"\']+)#',
         function( $matches ) {
@@ -86,7 +89,7 @@ function fhttps_convert_urls_to_https( $content ) {
 }
 
 // helper function: convert inline styles with url() to https
-function fhttps_convert_inline_styles_to_https( $content ) {
+function force_https_convert_inline_styles_to_https( $content ) {
     return preg_replace_callback(
         '#(<[^>]+(?:style)=["\'][^>]*?url\((http://)([^"\']+)\))#',
         function( $matches ) {
@@ -97,35 +100,41 @@ function fhttps_convert_inline_styles_to_https( $content ) {
 }
 
 // enforce https for text widget content
-add_filter( 'widget_text', function( $content ) {
+add_filter( 'widget_text', 'force_https_fix_widget_text', 20 );
+function force_https_fix_widget_text( $content ) {
     return str_replace( 'http://', 'https://', $content );
-}, 20 );
+}
 
 // enforce https for widget text content in newer wordpress versions
-add_filter( 'widget_text_content', function( $content ) {
+add_filter( 'widget_text_content', 'force_https_fix_widget_text_content', 20 );
+function force_https_fix_widget_text_content( $content ) {
     return str_replace( 'http://', 'https://', $content );
-}, 20 );
+}
 
 // apply https to all urls in custom menus
-add_filter( 'nav_menu_link_attributes', function( $atts ) {
+add_filter( 'nav_menu_link_attributes', 'force_https_fix_menu_links', 20 );
+function force_https_fix_menu_links( $atts ) {
     if ( isset( $atts['href'] ) && strpos( $atts['href'], 'http://' ) === 0 ) {
         $atts['href'] = set_url_scheme( $atts['href'], 'https' );
     }
     return $atts;
-}, 20 );
+}
 
 // enforce https for oembed urls
-add_filter( 'embed_oembed_html', function( $html ) {
+add_filter( 'embed_oembed_html', 'force_https_fix_oembed_html', 20 );
+function force_https_fix_oembed_html( $html ) {
     return str_replace( 'http://', 'https://', $html );
-}, 20 );
+}
 
 // enforce https for any urls used in shortcodes
-add_filter( 'do_shortcode_tag', function( $output ) {
+add_filter( 'do_shortcode_tag', 'force_https_fix_shortcode_urls', 20 );
+function force_https_fix_shortcode_urls( $output ) {
     return str_replace( 'http://', 'https://', $output );
-}, 20 );
+}
 
 // enforce https on wp_resource_hints
-add_filter( 'wp_resource_hints', function( $urls ) {
+add_filter( 'wp_resource_hints', 'force_https_fix_resource_hints', 20 );
+function force_https_fix_resource_hints( $urls ) {
     if ( is_array( $urls ) ) {
         foreach ( $urls as &$url ) {
             if ( is_array( $url ) && isset( $url['href'] ) ) {
@@ -136,10 +145,11 @@ add_filter( 'wp_resource_hints', function( $urls ) {
         }
     }
     return $urls;
-}, 20 );
+}
 
 // enforce https on attachment metadata
-add_filter( 'wp_get_attachment_metadata', function( $data ) {
+add_filter( 'wp_get_attachment_metadata', 'force_https_fix_attachment_metadata', 20 );
+function force_https_fix_attachment_metadata( $data ) {
     if ( isset( $data['file'] ) ) {
         $data['file'] = str_replace( 'http://', 'https://', $data['file'] );
     }
@@ -151,28 +161,30 @@ add_filter( 'wp_get_attachment_metadata', function( $data ) {
         }
     }
     return $data;
-}, 20 );
+}
 
 // enforce https on image srcsets
-add_filter( 'wp_calculate_image_srcset', function( $sources ) {
+add_filter( 'wp_calculate_image_srcset', 'force_https_fix_image_srcsets', 20 );
+function force_https_fix_image_srcsets( $sources ) {
     foreach ( $sources as &$source ) {
         if ( isset( $source['url'] ) ) {
             $source['url'] = str_replace( 'http://', 'https://', $source['url'] );
         }
     }
     return $sources;
-}, 20 );
+}
 
 // enforce https on custom logo html
-add_filter( 'get_custom_logo', function( $html ) {
+add_filter( 'get_custom_logo', 'force_https_fix_custom_logo', 20 );
+function force_https_fix_custom_logo( $html ) {
     return str_replace( 'http://', 'https://', $html );
-}, 20 );
+}
 
 // enforce https for login/logout redirect urls
-add_filter( 'login_redirect', 'fhttps_securize_url', 20 );
-add_filter( 'logout_redirect', 'fhttps_securize_url', 20 );
+add_filter( 'login_redirect', 'force_https_securize_url', 20 );
+add_filter( 'logout_redirect', 'force_https_securize_url', 20 );
 
 // ensure redirects are https
-add_filter( 'wp_redirect', 'fhttps_securize_url', 20 );
+add_filter( 'wp_redirect', 'force_https_securize_url', 20 );
 
 // Ref: ChatGPT
